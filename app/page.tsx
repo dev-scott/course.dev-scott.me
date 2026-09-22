965757e53b9049cc7de9506fce249036e9bcf39e
Dev-scott <sadoscott25@gmail.com>
2026-09-22 20:02:05 +0100

    feat: setup my project

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import NonDashboardNavbar from "@/components/ui/NonDashboardNavbar";
 import Image from "next/image";
+import Landing from "./(nondashboard)/landing";
 
 export default function Home() {
   return (
-    <div className="font-montserrat flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black">
-      Hi s
+    <div className="">
+      <NonDashboardNavbar />
+      <main className="">
+        <Landing />
+      </main>
     </div>
   );
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,1 +1,7 @@
-const ello = 9;
+"use client";
+
+import React from "react";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
